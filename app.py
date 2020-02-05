c35f9079ff49454f921ef81aeb7a062dcdb75771
ultan kearns <ultankearns@gmail.com>
2020-02-05 19:03:06 +0000

    Started on basic UI

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-import cv2
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+class MainMenu(GridLayout):
+    def __init__(self,**kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+        self.cols = 1
+        self. rows = 4
+        self.add_widget(Label(text= "WELCOME TO SIMON! - THIS GAME WILL TEST YOUR MEMORY"))
+        self.add_widget(Button(text='Start Game '))
+        self.add_widget(Button(text='Options'))
+        self.add_widget(Button(text='Quit Game'))
 
-def app():
-    print("Test")
-    video_capture = cv2.VideoCapture(0)
-    while True:
-        ret,frame = video_capture.read()
-        cv2.imshow('My Video', frame)
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
-
-app();
+class Menu(App):
+    def build(self):
+        return MainMenu();
+if __name__ == '__main__':
+    Menu().run()
